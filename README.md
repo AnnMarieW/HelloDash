65b38306b983c1378b49c58ddd0a78204114b6d5
Ann Marie Ward <amward@fastmail.us>
2021-02-23 13:29:44 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # HelloDash
-Example Plotly Dash Apps
+
+This app helps you create a theme for your app
+
+Choose from:
+ - 22 [Boostrap Bootswatch themes](https://www.bootstrapcdn.com/bootswatch/)
+ - 11 [Plotly Graph Templates](https://plotly.com/python/templates/)
+ - 18 [Plotly Graph Discrete Color Sequences](https://plotly.com/python/builtin-colorscales/#discrete-color-sequences)
+ - 94 [Plotly Graph Continuous Colorscales](https://plotly.com/python/builtin-colorscales/)
+ - Any background color selected from a colorpicker
+
+
+Please see the app at https://hellodash.pythonanywhere.com/
+
+
+![theme](https://user-images.githubusercontent.com/72614349/108897468-5f28f100-75d3-11eb-9f5f-095315cc1516.gif)
+
+
+These two images are the same app!  Only 5 lines are changed to set a different theme.
+
+
+
+|Light Theme app     | Dark Theme app |
+| ----------- | ----------- |
+| ![minty](https://user-images.githubusercontent.com/72614349/108880577-aa390900-75bf-11eb-8cb2-d246b342f4b5.png#thumbnail) | ![dark](https://user-images.githubusercontent.com/72614349/108880544-a1483780-75bf-11eb-913d-09c10adbe537.png#thumbnail) |
+| __Boostrap Theme:__ MINTY | DARKLY
+| __Graph Template:__ simple_white | plotly_dark|
+| __Graph Color Sequences:__ Pastel | Dark24|
+| __Graph Continuous Colorscales:__ darkmint | ice|
+| __App Background Color:__ #F3F6F3 | ""|
+
+------
---
+++ theme_explorer_sample_app.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+"""
+Sample app with two different themes
+"""
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import dash_bootstrap_components as dbc
+
+"""
+=====================================================================
+Change setting here to apply themes to app
+"""
+## App version 1  Minty theme
+
+external_stylesheets = [dbc.themes.MINTY]
+graph_template = "simple_white"
+graph_color_sequence = px.colors.qualitative.Pastel
+graph_continous_colorscale = "darkmint"
+background_color = "#F3F6F3"
+
+## App version 2 Dark theme
+## See github repo /assets/ for additional CSS for Dark themes
+
+# external_stylesheets = [dbc.themes.DARKLY]
+# graph_template = "plotly_dark"
+# graph_color_sequence = px.colors.qualitative.Dark24
+# graph_continous_colorscale = "ice"
+# background_color = ""
+# =====================================================================
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+df = px.data.gapminder()
+
+
+def make_dropdown(id, option_list, option_val=0):
+    return dcc.Dropdown(
+        id=id,
+        options=[{"label": str(i), "value": i} for i in option_list],
+        value=option_list[option_val],
+        clearable=False,
+        persistence_type="session",
+    )
+
+
+def make_checklist(id, option_list):
+    return dcc.Checklist(
+        id=id,
+        options=[{"label": i, "value": i} for i in option_list],
+        value=[option_list[0]],
+        labelStyle={"display": "inline-block"},
+        inputClassName="mr-2 ml-2",
+        persistence_type="session",
+    )
+
+
+def make_range_slider(id, slider_list, step=1):
+    return dcc.RangeSlider(
+        id=id,
+        min=slider_list[0],
+        max=slider_list[-1],
+        step=step,
+        marks={int(i): str(i) for i in slider_list},
+        value=[slider_list[0], slider_list[-1]],
+        persistence_type="session",
+    )
+
+
+buttons = html.Div(
+    [
+        dbc.Button("Primary", color="primary", className="mr-1"),
+        dbc.Button("Secondary", color="secondary", className="mr-1"),
+        dbc.Button("Success", color="success", className="mr-1"),
+        dbc.Button("Warning", color="warning", className="mr-1"),
+        dbc.Button("Danger", color="danger", className="mr-1"),
+        dbc.Button("Info", color="info", className="mr-1"),
+        dbc.Button("Light", color="light", className="mr-1"),
+        dbc.Button("Dark", color="dark", className="mr-1"),
+        dbc.Button("Link", color="link"),
+    ]
+)
+
+
+controls = dbc.Card(
+    [
+        dbc.FormGroup(
+            [
+                dbc.Label("Select indicator (y-axis)"),
+                make_dropdown("indicator", ["gdpPercap", "lifeExp", "pop"]),
+            ]
+        ),
+        dbc.FormGroup(
+            [
+                dbc.Label("Select continents"),
+                make_checklist("continents", df.continent.unique()),
+            ]
+        ),
+        dbc.FormGroup(
+            [
+                dbc.Label("Select years"),
+                make_range_slider("slider_years", df.year.unique(), 5),
+            ]
+        ),
+        dbc.FormGroup([dbc.Label("Sample Buttons"), buttons]),
+    ],
+    className="m-4 p-2",
+)
+
+
+app.layout = dbc.Container(
+    [
+        html.H1("Theme Explorer App", className="bg-primary text-white",),
+        html.Hr(),
+        dbc.Row(
+            [
+                dbc.Col(dcc.Graph(id="line_chart"), md=6),
+                dbc.Col(dcc.Graph(id="scatter_chart"), md=6),
+            ]
+        ),
+        controls,
+        html.Hr(),
+    ],
+    id="layout_container",
+    fluid=True,
+    style={"backgroundColor": background_color},
+)
+
+
+@app.callback(
+    Output("line_chart", "figure"),
+    Output("scatter_chart", "figure"),
+    Input("indicator", "value"),
+    Input("continents", "value"),
+    Input("slider_years", "value"),
+)
+def update_line_chart(indicator, continents, years):
+    if continents == [] or indicator is None:
+        return {}, {}
+
+    dff = df[df.year.between(years[0], years[1])]
+    fig = px.line(
+        dff[dff.continent.isin(continents)],
+        x="year",
+        y=indicator,
+        color="country",
+        template=graph_template,
+        color_discrete_sequence=graph_color_sequence,
+    )
+    dff = df[df.year == years[1]]
+    fig2 = px.scatter(
+        dff[dff.continent.isin(continents)],
+        x="lifeExp",
+        y=indicator,
+        color="lifeExp",
+        template=graph_template,
+        color_continuous_scale=graph_continous_colorscale,
+        hover_data=["country", "year"],
+    )
+    return fig, fig2
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
